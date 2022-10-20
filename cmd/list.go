0bcef43bbaaca84ee78b080d2a6a72614ddeba58
Jay Yoo <jay.yoo@ruckusmarketing.com>
2022-10-20 15:26:56 -0400

    created file and project structure

---
+++ cmd/list.go
@@ -0,0 +1,10 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func CobraPrint(){
+	fmt.Println("cli print!")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"github.com/jaeyoony/cli_todo_list/cmd"
+
+)
+
+func main()  {
+	cmd.Execute()
+}
